cff797aa4b43017fadfca68662c4bdeab963a7d3
Shivangini Gupta <shivanginigupta573@gmail.com>
2026-05-16 22:12:21 +0530

    solution of today's contest 2228B

---
+++ CF/2228A.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+void solve(int n, vector<int>& w) {
+    sort(w.begin(), w.end());
+    int oper=0;
+    int l=0;
+    int r=w.size()-1;
+    
+    while (l<=r && w[l]==0) {
+        oper++;
+        l++;
+    }
+    
+    while (l<r) {
+        if (w[l]==1 && w[r]==2) {
+            oper++;
+            l++; 
+            r--;
+        } else {
+            break; 
+        }
+    }
+    if (l<=r) {
+        int remaining_elements=r-l+1;
+        oper += remaining_elements/3;
+    }
+    
+    cout <<oper<< endl;
+}
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vector<int>w(n);
+        for(int i=0;i<n;i++)
+        {
+            cin>>w[i];
+        }
+        solve(n,w);
+    }
+}
