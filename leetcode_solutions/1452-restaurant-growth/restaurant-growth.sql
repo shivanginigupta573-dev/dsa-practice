9e4ee0b072d4d84bf57f6a4c764f34e50eb88b43
Shivangini Gupta <shivanginigupta573@gmail.com>
2026-05-25 09:06:23 +0530

    Time: 427 ms (9.88%) | Memory: 0B (100.00%) - LeetSync

---
+++ leetcode_solutions/1452-restaurant-growth/restaurant-growth.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT  c1.visited_on, SUM(c2.amount) AS amount, ROUND(AVG(c2.amount), 2) AS average_amount FROM  (SELECT visited_on, SUM(amount) AS amount FROM Customer GROUP BY visited_on) c1 JOIN 
+    (SELECT visited_on, SUM(amount) AS amount FROM Customer GROUP BY visited_on) c2
+    ON DATEDIFF(c1.visited_on, c2.visited_on) BETWEEN 0 AND 6
+GROUP BY c1.visited_on
+HAVING COUNT(DISTINCT c2.visited_on) = 7
+ORDER BY c1.visited_on;
