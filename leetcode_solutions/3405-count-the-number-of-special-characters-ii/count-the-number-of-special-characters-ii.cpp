3ca783e2b45c6d55da83c0a6dd2c242a5f5803c7
Shivangini Gupta <shivanginigupta573@gmail.com>
2026-05-27 10:21:03 +0530

    Time: 82 ms (34.27%) | Memory: 25.4 MB (30.62%) - LeetSync

---
+++ leetcode_solutions/3405-count-the-number-of-special-characters-ii/count-the-number-of-special-characters-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int numberOfSpecialChars(string word) {
+        unordered_map<char, int> lastLower, firstUpper;
+        for (int i=0;i<word.size();i++) {
+            if (islower(word[i]))
+                lastLower[word[i]]=i; 
+            else if (isupper(word[i])&&firstUpper.find(word[i])==firstUpper.end())
+                firstUpper[word[i]]=i;          
+        }
+
+        int count=0;
+        for (auto& [ch,idx]:lastLower) {
+            char up=toupper(ch);
+            if (firstUpper.count(up)&&idx<firstUpper[up])
+                count++;
+        }
+        return count;
+    }
+};
