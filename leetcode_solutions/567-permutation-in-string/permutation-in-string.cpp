c0e58ec2bc1993635762b8f2f81acee572317acd
Shivangini Gupta <shivanginigupta573@gmail.com>
2026-06-12 19:15:40 +0530

    Time: 1051 ms (5.01%) | Memory: 60.6 MB (5.73%) - LeetSync

--- leetcode_solutions/567-permutation-in-string/permutation-in-string.cpp
+++ leetcode_solutions/567-permutation-in-string/permutation-in-string.cpp
@@ -1,27 +1,13 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        unordered_map<char,int>mp;
-        for(char c : s1) mp[c]++;
-
-        int n=s1.size(), m=s2.size();
-        if(n>m) 
-        return false;
-
-        int match=0;
-        for(int i=0;i<m;i++) {
-            if(--mp[s2[i]]>=0) 
-            match++;
-
-            if(i>=n) 
-            {
-                if(++mp[s2[i-n]]>0) 
-                match--;
-            }
-
-            if(match==n)
-             return true;
-        }
-        return false;
+      sort(s1.begin(), s1.end());
+    int n = s1.size(), m = s2.size();
+    for(int i = 0; i <= m - n; i++) {
+        string t = s2.substr(i, n);
+        sort(t.begin(), t.end());
+        if(t == s1) return true;
+    }
+    return false;
     }
 };
