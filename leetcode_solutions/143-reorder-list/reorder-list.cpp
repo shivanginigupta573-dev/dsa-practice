9101e62d82c056b2235711d91bbe748605d8b502
Shivangini Gupta <shivanginigupta573@gmail.com>
2026-06-20 23:31:10 +0530

    Time: 0 ms (100.00%) | Memory: 22.8 MB (77.34%) - LeetSync

---
+++ leetcode_solutions/143-reorder-list/reorder-list.cpp
@@ -0,0 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+     ListNode* reverseList(ListNode* head)
+     {
+        ListNode* prev=nullptr;
+        ListNode* curr=head;
+        ListNode * nextNode;
+        while(curr)
+        {
+            nextNode=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=nextNode;
+        }
+        return prev;
+     }
+    void reorderList(ListNode* head) {
+        ListNode* slow=head;
+        ListNode * fast=head;
+        //get the middle
+        while(fast && fast->next)
+        {
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        //reverse the second half
+        ListNode* secondHalf=reverseList(slow->next);
+        //break the connection now they become two independent lists
+        slow->next=nullptr;
+        //now merge the lists
+       ListNode* first=head;
+       ListNode* second=secondHalf;
+       while(second)
+       {
+           ListNode *t1=first->next;
+           ListNode *t2=second->next;
+           first->next=second;
+           second->next=t1;
+           first=t1;
+           second=t2;
+       }
+        
+    }
+};
