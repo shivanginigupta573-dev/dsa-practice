42a69e4ed10c5ddf1b71c655f7a75d94f0f17b21
Shivangini Gupta <shivanginigupta573@gmail.com>
2026-06-23 10:20:45 +0530

    Solution for 266A

---
+++ CF/266A.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+    int c=0;
+    for(int i=1;i<n;i++)
+    {
+      if(s[i-1]==s[i])
+      {
+        c++;
+      }
+    }
+    cout<<c<<endl;
+}
