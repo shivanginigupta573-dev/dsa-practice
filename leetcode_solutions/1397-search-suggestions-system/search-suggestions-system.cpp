72b1ba1f201967cacdf6628b78bad73e448e8826
Shivangini Gupta <shivanginigupta573@gmail.com>
2026-06-25 17:25:01 +0530

    Time: 458 ms (17.51%) | Memory: 182.6 MB (18.88%) - LeetSync

--- leetcode_solutions/1397-search-suggestions-system/search-suggestions-system.cpp
+++ leetcode_solutions/1397-search-suggestions-system/search-suggestions-system.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
     vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
-        vector<vector<string>>res;
         sort(products.begin(),products.end());
-        string prefix = "";
-        auto it = products.begin();
-
-        for (char c : searchWord) {
-            prefix += c;
-            it = lower_bound(it, products.end(), prefix); // starting from last pos
-            vector<string> tmp;
-            for (auto i = it; i < min(it + 3, products.end()) && i->substr(0, prefix.size()) == prefix; i++) {
-                tmp.push_back(*i);
+        vector<vector<string>>ans;
+        string prefix="";
+        for(char c:searchWord)
+        {
+            prefix+=c;
+            vector<string>curr;
+            for(string &p:products)
+            {
+                if(p.substr(0,prefix.size())==prefix)
+                {
+                    curr.push_back(p);
+                }
+                if(curr.size()==3)
+                break;
             }
-            res.push_back(tmp);
+            ans.push_back(curr);
         }
-        return res;
+
+       return ans; 
     }
 };
