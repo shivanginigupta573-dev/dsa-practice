a7d87b8f478ee7d004ae9a2556a82dd899c0fc33
Shivangini Gupta <shivanginigupta573@gmail.com>
2026-06-28 23:50:04 +0530

    Time: 64 ms (23.70%) | Memory: 94.1 MB (21.94%) - LeetSync

--- leetcode_solutions/435-non-overlapping-intervals/non-overlapping-intervals.cpp
+++ leetcode_solutions/435-non-overlapping-intervals/non-overlapping-intervals.cpp
@@ -1,30 +1,23 @@
 class Solution {
 public:
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-        //interval problem so greedy algorithm works iguess lets tryy
-        if(intervals.size()==0)
-        {
-            return 0;
-        }
-        sort(intervals.begin(),intervals.end(),[](vector<int>&a,vector<int>&b){
-            return a[1]<b[1];
-        });
+        sort(intervals.begin(),intervals.end());
         int n=intervals.size();
         int ans=0;
-        int last=intervals[0][1];
+        vector<int>interval=intervals[0];
         for(int i=1;i<n;i++)
         {
-            if(intervals[i][0]<last )
-            {
-                //remove that
+           if(intervals[i][0]<interval[1])//overlaps
+           {
                 ans++;
-            }
+                interval[0]=min(interval[0],intervals[i][0]);
+                interval[1]=min(interval[1],intervals[i][1]);
+           }
            else
            {
-            last=intervals[i][1];
+            interval=intervals[i];
            }
-        }
-      return ans;
-        
+        } 
+        return ans;
     }
 };
