4b825002b3274879689319206b9974744332620a
Shivangini Gupta <shivanginigupta573@gmail.com>
2026-06-29 19:28:00 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (82.04%) - LeetSync

---
+++ leetcode_solutions/2099-number-of-strings-that-appear-as-substrings-in-word/number-of-strings-that-appear-as-substrings-in-word.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numOfStrings(vector<string>& patterns, string word) {
+        int ans=0;
+        for(string s:patterns)
+        {
+           if(word.contains(s))
+           {
+            ans++;
+           }
+        }
+
+        return ans;
+    }
+};
