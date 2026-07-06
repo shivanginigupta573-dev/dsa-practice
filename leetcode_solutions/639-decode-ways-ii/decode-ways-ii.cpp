f3c64315954d2d9e4cac43b853db803c8e86a583
Shivangini Gupta <shivanginigupta573@gmail.com>
2026-07-06 14:18:58 +0530

    Time: 13 ms (66.50%) | Memory: 25.3 MB (66.83%) - LeetSync

--- leetcode_solutions/639-decode-ways-ii/decode-ways-ii.cpp
+++ leetcode_solutions/639-decode-ways-ii/decode-ways-ii.cpp
@@ -3,7 +3,7 @@ public:
     int const m=1e9+7;
     int numDecodings(string s) {
         int n=s.size();
-        if(n==0 || s[0]=='0')
+        if(n==0||s[0]=='0')
          return 0;
         vector<long long>dp(n+1,0);
         dp[0]=1;
@@ -24,11 +24,16 @@ public:
             if (prev == '*' && curr == '*') {
                 dp[i] = (dp[i] + 15LL * dp[i-2]) % m;
             } else if (prev == '*') {
-                if (curr <= '6') dp[i] = (dp[i] + 2LL * dp[i-2]) % m;
-                else dp[i] = (dp[i] + dp[i-2]) % m;
-            } else if (curr == '*') {
-                if (prev == '1') dp[i] = (dp[i] + 9LL * dp[i-2]) % m;
-                else if (prev == '2') dp[i] = (dp[i] + 6LL * dp[i-2]) % m;
+                if (curr <= '6')
+                 dp[i] = (dp[i] + 2LL * dp[i-2]) % m;
+                else 
+                 dp[i] = (dp[i] + dp[i-2]) % m;
+            } else if (curr == '*') 
+            {
+                if (prev == '1') 
+                 dp[i] = (dp[i] + 9LL * dp[i-2]) % m;
+                else if (prev == '2') 
+                 dp[i] = (dp[i] + 6LL * dp[i-2]) % m;
             } else {
                 int val = (prev - '0') * 10 + (curr - '0');
                 if (val >= 10 && val <= 26) {
