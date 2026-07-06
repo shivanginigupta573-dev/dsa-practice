e7a5c4eca0545685e2e95707b4e76cbe11163d73
Shivangini Gupta <shivanginigupta573@gmail.com>
2026-07-06 13:46:54 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (36.45%) - LeetSync

---
+++ leetcode_solutions/91-decode-ways/decode-ways.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        int n=s.length();
+        if(n==0 || s[0]=='0')
+         return 0;
+         vector<int>dp(n+1,0);
+         dp[0]=1;
+         dp[1]=1;
+         for(int i=2;i<=n;i++)
+         {
+            if(s[i-1]!='0')
+            dp[i]+=dp[i-1];
+            int twodigit=stoi(s.substr(i-2,2));
+            if(twodigit>=10 && twodigit<=26)
+            {
+                dp[i]+=dp[i-2];
+            }
+         }
+       return dp[n]; 
+    }
+};
