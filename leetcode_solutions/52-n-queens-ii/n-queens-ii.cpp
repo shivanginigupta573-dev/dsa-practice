b7da0218cab668d8b9111d4be5b292d80f0fa8d7
Shivangini Gupta <shivanginigupta573@gmail.com>
2026-07-06 11:35:50 +0530

    Time: 3 ms (39.14%) | Memory: 9 MB (23.42%) - LeetSync

--- leetcode_solutions/52-n-queens-ii/n-queens-ii.cpp
+++ leetcode_solutions/52-n-queens-ii/n-queens-ii.cpp
@@ -1,24 +1,46 @@
 class Solution {
 public:
-    int total=0;
-    void dfs(int row,int n,int cols,int d1,int d2)
-    {
+      vector<string>board;
+      vector<vector<string>>ans;
+      bool isSafe(int row,int col,int n)
+      {
+        for(int i=0;i<row;i++)
+        {
+            if(board[i][col]=='Q')
+            return false;
+        }
+        for(int i=row-1,j=col-1;i>=0 && j>=0;j-- , i--)
+        {
+            if(board[i][j]=='Q')
+            return false;
+        }
+         for(int i=row-1 , j=col+1;i>=0 && j<n;i--,j++)
+         if(board[i][j]=='Q')
+          return false;
+        return true;
+      }
+      void solve(int row,int n)
+      {
         if(row==n)
         {
-            total++;
+            ans.push_back(board);
             return;
         }
-        int available=((1<<n)-1)& ~(cols|d1|d2);
-        while(available)
+       //put q in all rows
+        for(int col=0;col<n;col++)
         {
-            int pos=available &(-available);
-            available &=available-1;
-            dfs(row+1,n,cols|pos,(d1|pos)<<1,(d2|pos)>>1);
+            if(isSafe(row,col,n)){
+            board[row][col]='Q';
+            solve(row+1,n);
+            board[row][col]='.';
+            }
         }
-    }
+      }
+
     int totalNQueens(int n) {
-        dfs(0,n,0,0,0);
-        return total;
+       board=vector<string>(n,string(n,'.'));
+        solve(0,n);
+        return ans.size();
         
     }
 };
