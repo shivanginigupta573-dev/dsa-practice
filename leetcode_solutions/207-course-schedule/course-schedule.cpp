b419ab200df4866c08afa60a8e31644803843f60
Shivangini Gupta <shivanginigupta573@gmail.com>
2026-07-14 17:26:56 +0530

    Time: 7 ms (45.21%) | Memory: 19.3 MB (55.25%) - LeetSync

--- leetcode_solutions/207-course-schedule/course-schedule.cpp
+++ leetcode_solutions/207-course-schedule/course-schedule.cpp
@@ -3,30 +3,32 @@ public:
     bool dfs(int node,vector<vector<int>>&adj,vector<int>&vis)
     {
         vis[node]=1;
+        //now check neighbours
         for(auto & nei:adj[node])
         {
             if(vis[nei]==0)
             {
-                if(! dfs(nei,adj,vis))
+                if(!dfs(nei,adj,vis))
                 return false;
             }
-           else if(vis [nei]==1)
+           else if(vis[nei]==1)//cycle is found same node visited again in the path
             {
-                //cycle found;
                 return false;
             }
+
         }
         vis[node]=2;
         return true;
+
     }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>>adj(numCourses);
+        
         vector<int>vis(numCourses,0);
-        for(auto &pre:prerequisites)
+        for(auto & pre:prerequisites)
         {
             adj[pre[0]].push_back(pre[1]);
         }
-        //now check for each we need to check for cycle
         for(int i =0;i<numCourses;i++)
         {
             if(vis[i]==0)
@@ -36,5 +38,6 @@ public:
             }
         }
       return true;
+        
     }
 };
