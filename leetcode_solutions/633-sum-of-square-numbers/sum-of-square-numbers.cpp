df5e8c6836bd7bc980932bccd21940a551c27406
Shivangini Gupta <shivanginigupta573@gmail.com>
2026-07-16 20:46:18 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (55.47%) - LeetSync

---
+++ leetcode_solutions/633-sum-of-square-numbers/sum-of-square-numbers.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+       
+        long long a,b;
+        a=0;
+        b=(sqrt(c));
+        while(a<=b)
+        {
+         long long sum=a*a+b*b;
+
+            if(sum==c)
+                return true;
+            else if(sum<c)
+                a++;
+            else
+                b--;
+        }
+
+        return false;
+    }
+};
