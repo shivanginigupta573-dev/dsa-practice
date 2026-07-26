18d46d4d3a1a3f5f9c47edd888565fa7ae69514a
Shivangini Gupta <shivanginigupta573@gmail.com>
2026-07-26 17:56:29 +0530

    Time: 28 ms (5.15%) | Memory: 23 MB (51.84%) - LeetSync

--- leetcode_solutions/907-koko-eating-bananas/koko-eating-bananas.cpp
+++ leetcode_solutions/907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,34 +1,35 @@
 class Solution {
 public:
     int minEatingSpeed(vector<int>& piles, int h) {
-        int i, max = 0;
-        for (i = 0; i < piles.size(); i++) {
-            if (max < piles[i])  {
-                max = piles[i];
+        //search space of k
+        int beg=1,end=*max_element(piles.begin(),piles.end());
+        int ans=INT_MAX;
+        while(beg<=end)
+        {
+            int mid=beg+(end-beg)/2;
+           long long hrs=helper(piles,mid);
+            if(hrs<=h)
+            {
+                ans=min(ans,mid);
+                end=mid-1;
             }
-        }
-        int low = 1, high = max, ans = 0;
-        while (low <= high) {
-            int mid = low + (high - low) /2;
-            long long totalhrs = helper(piles, mid);
-            if (totalhrs <= h) {
-                ans = mid;
-                high = mid - 1;
-            } else {
-                low = mid + 1;
+            else if(hrs>h)
+            {
+                beg=mid+1;
             }
+           
         }
-
-        return ans;
+        
+    return ans;
+        
     }
-
-    long long helper(vector<int>& piles, int h) {
-        long long totalhrs = 0;
-        for (int i = 0; i < piles.size(); i++) {
-            totalhrs += (piles[i] + h - 1) / h;
+    long long helper(vector<int>&piles,int k)
+    {
+        long long  hours=0;
+        for(int p:piles)
+        {
+            hours+=ceil((double)p/k);
         }
-        return totalhrs;
+        return hours;
     }
 };
-
-
