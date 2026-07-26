a8a8524615477c451654ef9b2ee23c2eaf27331d
Shivangini Gupta <shivanginigupta573@gmail.com>
2026-07-26 23:30:46 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (79.56%) - LeetSync

---
+++ leetcode_solutions/4353-largest-integer-with-given-digit-sum/largest-integer-with-given-digit-sum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+  
+    int largestInteger(int n, int s) {
+        if(s==0)
+        return 0;
+       if(s>9*n)
+       {
+        return -1;
+       }
+       int ans=0;
+       for(
+        int i=0;i<n;i++)
+       {
+        int d=min(9,s);
+        ans=ans*10+d;
+        s-=d;
+       }
+
+      return ans;
+        
+    }
+};
